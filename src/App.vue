02f71a43423f3b1cc6bbbf206710e5da1f93efbe
xuhao <445739415@qq.com>
2019-10-11 16:39:37 +0800

    Add Element plugin

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,33 @@
 <template>
   <div id="app">
-    <v-header></v-header>
-    <v-navigate></v-navigate>
-    <div  class="g-body">
-      <router-view class="g-wrapper"/>
-
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
-      <!-- <router-view class="g-body"/> -->
   </div>
 </template>
+
 <script>
-import vHeader from '@/components/layout/header.vue'
-import vNavigate from '@/components/layout/navigate.vue'
+
 export default {
   name: 'app',
   components: {
-    vHeader,
-    vNavigate,
-  },
-  data() {
-    return {
-
-    }
   }
 }
 </script>
-<style lang="scss">
-* {
-  margin: 0;
-  box-sizing: border-box;
-}
-html, body {
-  width: 100%;
-  height: 100%;
-}
-#app {
-  height: 100%;
-}
-.g-body {
-  // box-sizing: content-box;
-  // padding: 60px 0 0 200px;
-  // height: 100%;
-  position: absolute;
-  top: 60px;
-  left: 200px;
-  right: 0;
-  bottom: 0;
-  background: pink;
-  // min-width: 900px;
-  overflow-x: auto;
-}
-.g-wrapper {
-  position: relative;
-  background: red;
-  width: 1000px;
-  min-width: 900px;
-  // overflow-x: auto;
-}
 
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
